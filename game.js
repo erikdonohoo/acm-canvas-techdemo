85aa221f9f38e0da4ad9773a4d3b575d594eff1f
Erik Donohoo <erikdonohoo@me.com>
2013-02-12 17:29:00 -0700

    Added files for techdemo

---
+++ game.js
@@ -0,0 +1,7 @@
+// Canvas Demo
+
+console.log('*** DEMO START ***');
+var c = document.getElementById('canvas');
+//console.log(c); // Prints out element from the screen
+var canvas = c.getContext('2d');
+console.log(canvas);
